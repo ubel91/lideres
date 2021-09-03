417742d3ff2ac1290f16db85405af7681a0b07a8
Ubel Angel <ubelangelfonseca@gmail.com>
2021-09-03 14:57:19 -0400

    customice viewer and editor

--- assets/js/unidad.js
+++ assets/js/unidad.js
@@ -20,89 +20,129 @@ pdfjsLib.workerSrc = pdfjsWorker
 const loadingTask = pdfjsLib.getDocument(pdf);
 
 var pdfDoc = null,
+    pagesBase64 = [],
     pageNum = 1,
     pageRendering = false,
     pageNumPending = null,
     scale = 1,
+    editorVisible = false,
     canvas = document.getElementById('canvasFlip'),
     ctx = canvas.getContext('2d');
 
-    function renderPage(num) {
-        pageRendering = true;
-        // Using promise to fetch the page
-        pdfDoc.getPage(num).then(function(page) {
-          var viewport = page.getViewport({scale: scale});
-          canvas.height = viewport.height;
-          canvas.width = viewport.width;
-      
-          // Render PDF page into canvas context
-          var renderContext = {
+function renderPage(num) {
+    pageRendering = true;
+    // Using promise to fetch the page
+    pdfDoc.getPage(num).then(function (page) {
+        var viewport = page.getViewport({ scale: scale });
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        // Render PDF page into canvas context
+        var renderContext = {
             canvasContext: ctx,
             viewport: viewport
-          };
-          var renderTask = page.render(renderContext);
-      
-          // Wait for rendering to finish
-          renderTask.promise.then(function() {
+        };
+        var renderTask = page.render(renderContext);
+
+        // Wait for rendering to finish
+        renderTask.promise.then(function () {
             pageRendering = false;
             if (pageNumPending !== null) {
-              // New page rendering is pending
-              renderPage(pageNumPending);
-              pageNumPending = null;
+                // New page rendering is pending
+                renderPage(pageNumPending);
+                pageNumPending = null;
             }
-          });
+            pagesBase64[num] = {
+                img: canvas.toDataURL('image/jpeg'),
+                width: viewport.width,
+                height: viewport.height,
+            };
         });
-          // Update page counters
-  document.getElementById('page_num').textContent = num;
+    });
+    // Update page counters
+    document.getElementById('page_num').textContent = num;
 }
-
 /**
  * If another page rendering in progress, waits until the rendering is
  * finised. Otherwise, executes rendering immediately.
  */
- function queueRenderPage(num) {
+function queueRenderPage(num) {
     if (pageRendering) {
-      pageNumPending = num;
+        pageNumPending = num;
     } else {
-      renderPage(num);
+        renderPage(num);
     }
-  }
+}
 
-  /**
- * Displays previous page.
- */
+/**
+* Displays previous page.
+*/
 function onPrevPage() {
     if (pageNum <= 1) {
-      return;
+        return;
     }
     pageNum--;
     queueRenderPage(pageNum);
-  }
-  document.getElementById('prev').addEventListener('click', onPrevPage);
+}
+document.getElementById('prev').addEventListener('click', onPrevPage);
 
-  /**
- * Displays next page.
- */
+/**
+* Displays next page.
+*/
 function onNextPage() {
     if (pageNum >= pdfDoc.numPages) {
-      return;
+        return;
     }
     pageNum++;
     queueRenderPage(pageNum);
-  }
-  document.getElementById('next').addEventListener('click', onNextPage);
+}
+document.getElementById('next').addEventListener('click', onNextPage);
 
 
-  /**
- * Asynchronously downloads PDF.
- */
-pdfjsLib.getDocument(pdf).promise.then(function(pdfDoc_) {
+/**
+* Asynchronously downloads PDF.
+*/
+pdfjsLib.getDocument(pdf).promise.then(function (pdfDoc_) {
     pdfDoc = pdfDoc_;
     document.getElementById('page_count').textContent = pdfDoc.numPages;
-  
+
     // Initial/first page rendering
     renderPage(pageNum);
-  });
+});
+
+document.getElementById('editor').addEventListener('click', function (event) {
+
+
+    if (editorVisible) {
+        $('#canvasFlip').fadeIn();
+        $('#tui-image-editor').fadeOut();
+        editorVisible = false;
+    } else {
+        editorVisible = true;
+        const ImageEditor = require('tui-image-editor');
+        $('#canvasFlip').fadeOut();
+        $('#tui-image-editor').fadeIn();
+        const instance = new ImageEditor(document.querySelector('#tui-image-editor'), {
+            includeUI: {
+                loadImage: {
+                    path: pagesBase64[pageNum].img,
+                    name: 'SampleImage',
+                },
+                locale: translate,
+                theme: customTheme,
+                // initMenu: 'filter',
+                menuBarPosition: 'left',
+
+            },
+            // cssMaxWidth: pagesBase64[pageNum].width / 2,
+            // cssMaxHeight: pagesBase64[pageNum].height / 2,
+            selectionStyle: {
+                cornerSize: 20,
+                rotatingPointOffset: 70,
+            },
+        });
+    }
+});
 
 
 // const Routing = require('./routing');
@@ -796,7 +836,7 @@ pdfjsLib.getDocument(pdf).promise.then(function(pdfDoc_) {
 //             $pageFlip.width(horizontalWidth);
 //         }
 //         $pageFlip.height(newHeight);
-       
+
 
 //     } else {
 //         let currentPageFlip = flip.leftPage;
