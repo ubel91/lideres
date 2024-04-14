34dc4e825f130b93bb2352b7fee4a56e82203748
ubel91 <ubelfonsecacedeno@gmail.com>
2024-04-14 02:54:38 -0400

    changes

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -5,7 +5,7 @@ namespace Deployer;
 require_once 'recipe/symfony4.php';
 require_once 'recipe/yarn.php';
 
-set('repository', 'https://github.com/uafonseca/lideres-development.git');
+set('repository', 'https://github.com/ubel91/lideres.git');
 
 set('application', 'lideres');
 
